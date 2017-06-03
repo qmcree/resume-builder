1906e273b46bf2fd2e395a853fd746e5b7748719
Quentin McRee <qmcree@gmail.com>
2017-06-03 14:07:05 -0500

    Added Django project and app barebones.

--- resume_builder/resume_builder/urls.py
+++ resume_builder/resume_builder/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+"""resume_builder URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
--- resume_builder/resume_builder/wsgi.py
+++ resume_builder/resume_builder/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project project.
+WSGI config for resume_builder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_builder.settings")
 
 application = get_wsgi_application()
