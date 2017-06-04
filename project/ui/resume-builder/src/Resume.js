7c56ad099f36c3223345e8a53dbff791c89022c1
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-04 14:00:21 -0500

    Create resume preview page

--- project/ui/resume-builder/src/Resume.js
+++ project/ui/resume-builder/src/Resume.js
@@ -22,14 +22,13 @@ class Resume extends React.Component {
         <div className="row resume-header">
           <div className="col-sm-12">
             <h1>Ellis B. Redding</h1>
-            <br/>
           </div>
         </div>
 
         <div className="row">
           <div className="col-sm-3">
-            <p>(555) 555-5555</p>
-            <p>email@gmail.com</p>
+            <p>(555) 555-5555<br/>
+            email@gmail.com</p>
             <br/>
             <h5>Skills</h5>
             <ul className="side-resume">
@@ -46,6 +45,12 @@ class Resume extends React.Component {
               <li>Team Player</li>
               <li>Self Monitoring</li>
             </ul>
+            <br/>
+            <br/>
+            <h5>Education</h5>
+            <p>Windham School District<br/>
+            Huntsville, TX200<br/>
+            GED</p>
           </div>
 
           <div className="col-sm-9">   
@@ -53,18 +58,33 @@ class Resume extends React.Component {
             in consistently producing and presenting well-prepared food while keeping high sanitation standards in 
             check.
             </p>
-            <br/>
             <h2>Professional Experience</h2>
             <h5>Cook</h5>
             <ul>
               <li>Clean and inspect gallery equipment, kitchen appliances, and work areas to ensure cleanliness and 
               functional operation.</li>
-              <li>Clean and inspect gallery equipment, kitchen appliances, and work areas to ensure cleanliness and 
-              functional operation.</li>
-              <li>Clean and inspect gallery equipment, kitchen appliances, and work areas to ensure cleanliness and 
-              functional operation.</li>
-              <li>Clean and inspect gallery equipment, kitchen appliances, and work areas to ensure cleanliness and 
-              functional operation.</li>
+              <li>Direct activities of one or more workers who assist in preparing and serving meals.</li>
+              <li>Wash pots, pans, dishes, utensils, or other cooking equipment.</li>
+              <li>Compile and maintain records of food use and expenditures.</li>
+            </ul>
+            <h5>Home Health Aide</h5>
+            <ul>
+              <li>Plan, purchase, prepare, or serve meals to patients or other family members, according to 
+              prescribed diets.</li>
+              <li>Perform a variety of duties as requested by client, such as obtaining household supplies 
+              or running errands.</li>
+              <li>Accompany clients to doctor's offices or on other trips outside the home, providing
+              transportation, assistance, and companionship.</li>
+              <li>Change dressings.</li>
+            </ul>
+            <h5>Cashier</h5>
+            <ul>
+              <li>Receive payment by cash, check, credit cards, vouchers, or automatic debits.</li>
+              <li>Assist customers by providing information and resolving their complaints.</li>
+              <li>Maintain clean and orderly checkout areas and complete other general cleaning
+              duties, such as mopping floors and emptying trash cans.</li>
+              <li>Calculate total payments rceived during a time period, and reconcile this with
+              total sales.</li>
             </ul>
           </div>
 
